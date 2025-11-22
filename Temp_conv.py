784a158b6eb72f260033e638d74da68a21f34963
Sunil2409 <esunilkumar2409@gmail.com>
2025-11-22 21:34:42 +0530

    Structured version of Temperature Converter

--- Temp_conv.py
+++ Temp_conv.py
@@ -1,31 +1,48 @@
-def menu():
+def display_menu():
     print("Please select the temperature converter you would like to use.")
-    Menu = int(input("1. Celsius to Fahrenheit \n" "2. Fahrenheit to Celsius\n"))
+    menu = int(input("1. Celsius to Fahrenheit \n" "2. Fahrenheit to Celsius\n"))
     print("------------------------------------------------------------------------")
-    return Menu
+    return menu
 
 def inputfromuser():
     temperature = float(input("Please enter the value of temperature: "))
     print("------------------------------------------------------------------------")
     return temperature
 
+def c_to_f():
+    C = inputfromuser()
+    F = (C * (9/5)) + 32
+    print(f"The Celsius is {C} and the converted Fahrenheit is {F:.2f}")
+    print("------------------------------------------------------------------------")
+
+def f_to_c():
+    F = inputfromuser()
+    C = (F - 32) * 5/9
+    print(f"The Fahrenheit is {F} and the converted Celsius is {C:.2f}")
+    print("------------------------------------------------------------------------")
+
 def conversion():
-    choice = menu()
+    choice = display_menu()
+    
     if choice == 1:
         #Celsius → Fahrenheit
-        C = inputfromuser()
-        F = (C * (9/5)) + 32
-        print(f"The Celsius is {C} and the converted Fahrenheit is {F:.2f}")
-        print("------------------------------------------------------------------------")
-
-    if choice == 2:
+        c_to_f()
+   
+    elif choice == 2:
         #Fahrenheit → Celsius
-        F = inputfromuser()
-        C = (F - 32) * 5/9
-        print(f"The Fahrenheit is {F} and the converted Celsius is {C:.2f}")
-        print("------------------------------------------------------------------------")
+        f_to_c()
 
-conversion()
+    else:
+        print("Invalid choice")
+
+def main():
+    continuation = "y"
+    while continuation.lower()=="y":
+        conversion()
+        continuation = input("Do you want to continue? (y/n): ")
+
+if __name__ == "__main__":
+    main()
 
 
 

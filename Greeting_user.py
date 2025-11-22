1df5564465d170bc9fb05b4c288970284fbd78fc
Sunil2409 <esunilkumar2409@gmail.com>
2025-11-22 23:30:24 +0530

    Structure version of Greeting User

--- Greeting_user.py
+++ Greeting_user.py
@@ -1,8 +1,18 @@
-Name = input("Enter your name: ")
-Age = input("Enter your Age: ")
-Language = input("Enter your Language: ")
+def getuserdetails():
+    Name = input("Enter your name: ")
+    Age = input("Enter your Age: ")
+    Language = input("Enter your Language: ")
+    return Name, Age, Language
 
-print("-----------------------------")
-print(f"Hello {Name}! 👋 \nYou are {Age} years old. \nYou're Favourite Language is {Language}.\nHave a great Day! ✨")
-print("-----------------------------")
+def greetuser(Name,Age,Language):
+    print("-----------------------------")
+    print(f"Hello {Name}! 👋 \nYou are {Age} years old. \nYou're Favourite Language is {Language}.\nHave a great Day! ✨")
+    print("-----------------------------")
 
+def main():
+    Name, Age, Language = getuserdetails()
+    greetuser(Name, Age, Language)
+    return
+
+if __name__ == "__main__":
+    main()

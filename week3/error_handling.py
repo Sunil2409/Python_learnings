d46a60ce837898223b3db4ec02103334f9300ac3
Sunil2409 <esunilkumar2409@gmail.com>
2025-11-28 18:02:48 +0530

    Initial Commit

---
+++ week3/error_handling.py
@@ -0,0 +1,17 @@
+def div():
+    try:
+        Num1 = int(input("Enter Number1: "))
+        Num2 = int(input("Enter Number2: "))
+        result = Num1/Num2
+    except ValueError:
+        print("Enter only Number")
+    except ZeroDivisionError:
+        print("Enter only positive number")
+    else:
+        return result
+    finally:
+        print("Please verify the output.")
+
+div()
+
+
